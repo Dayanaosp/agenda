222b89518620d0972b855b5933764fd86287daad
Dayana <2520221013@coreducacion.edu.co>
2024-10-19 11:30:57 -0500

    Se agrego el archivo conexion.php

---
+++ modelo/conexion.php
@@ -0,0 +1,26 @@
+<?php
+
+class Conexion{
+    private $host = 'localhost';
+    private $db_name = 'agenda';
+    private $username = 'root';
+    private $password = '12345678';
+    public $conn;
+
+    public function getConexion(){
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->exec("set names UTF8");
+        } catch(\Throwable $exception){
+            echo "Error de conexion".$exception->getMessage();
+        }
+
+        return $this->conn;
+
+    }
+
+}
+
+?>
